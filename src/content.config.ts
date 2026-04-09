b55b3e87364aa5ab8f58d2ea4b7907d9e29a842d
cassigs <ccgall@gmail.com>
2026-04-09 15:00:23 -0700

    Install base theme

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,32 @@
-import { defineCollection } from 'astro:content';
-import { z } from 'astro/zod';
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+import { glob } from "astro/loaders";
 
-const blogCollection = defineCollection({
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    description: z.string(),
-    author: z.string(),
-    image: image(),
-    imageAlt: z.string(),
-    tags: z.array(z.string()),
-  }),
+const posts = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/posts" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      publicationDate: z.date(),
+      image: image().optional(),
+      imageAlt: z.string().optional(),
+      tags: z.array(z.string()).optional(),
+    }),
 });
 
-export const collections = {
-  blog: blogCollection,
-};
+const projects = defineCollection({
+  loader: glob({
+    pattern: "**/[^_]*.{md,mdx}",
+    base: "./src/content/projects",
+  }),
+  schema: () =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      publicationDate: z.date().optional(),
+      href: z.string(),
+    }),
+});
+
+export const collections = { posts, projects };
